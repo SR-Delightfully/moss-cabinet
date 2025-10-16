1570fcca87ef77b6ce99bb87ccebbd29d5f4383e
SR-Delightfully <033.s.robinson@gmail.com>
2025-10-16 11:30:53 -0400

    added a global styles css file with templates and root variables to help implement a consistent style for the entire website and support multiple themes.

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?></title>
     <!-- TODO: include your CSS files here -->
+    <link rel="stylesheet" href="./public/assets/css/00-Global-Styles.css">
 </head>
 
-<body>
+<body data-theme="lights-off">
