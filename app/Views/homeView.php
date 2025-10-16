baad3849d93858d23314e4f617d2b6e58387f0c0
SR-Delightfully <033.s.robinson@gmail.com>
2025-10-16 12:04:10 -0400

    adding more helpful classes :)

--- app/Views/homeView.php
+++ app/Views/homeView.php
@@ -5,16 +5,16 @@ use App\Helpers\ViewHelper;
 $page_title = 'Home';
 ViewHelper::loadHeader($page_title);
 ?>
+<div class="page center border-2">
+    <h1>Slim Framework-based MVC Application</h1>
+    <p>This is a simple MVC application built with Slim Framework.</p>
 
-<h1>Slim Framework-based MVC Application</h1>
-<p>This is a simple MVC application built with Slim Framework.</p>
+    <p>This app uses a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.</p>
 
-<p>This app uses a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
 
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
-
-
+</div>
 <?php
 
 ViewHelper::loadJsScripts();
