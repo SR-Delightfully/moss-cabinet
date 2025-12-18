b2a1fff925748a71cf7ca9262584b74b8077f3fe
root <root@H.ash_a9236.org>
2025-12-17 23:11:34 -0500

    idkkkk anymoreeee

---
+++ app/Views/admin/components/topBar.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Helpers\UserContext;
+use App\Helpers\LocalizationHelper;
+
+$currentUser = UserContext::getCurrentUser();
+
+$tabs = [
+    'collections' => ['key' => 'collections'],
+    'categories'  => ['key' => 'categories'],
+    'products'    => ['key' => 'products'],
+];
+
+if (UserContext::isAdmin()) {
+    $tabs = ['admin' => ['key' => 'admin']] + $tabs;
+}
+?>
+
+<header id="admin-top-bar">
+    <nav id="nav-bar" class="display-flex-row">
+        <a id="brand" class="bilbo-swash-caps-regular" href="home">
+            <h1>M<i class="corinthis-bold">oss</i> C<i class="corinthis-bold">abinet</i></h1>
+        </a>
+
+        <img class="sparkle-icon" src="https://svgsilh.com/svg/35893.svg" alt="sparkle">
+
+        <div id="nav-bar-content" class="display-flex-row">
+            <ul id="tabs">
+                <?php foreach ($tabs as $key => $tab): ?>
+                    <li class="tab" id="<?= htmlspecialchars($key) ?>">
+                        <a href="/<?= htmlspecialchars($tab['key']) ?>">
+                            <span class="tab-label">
+                                <?= LocalizationHelper::get("navbar_content.$key") ?>
+                            </span>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
+            <div id="nav-bar-user" class="display-flex-col">
+                <?php if ($currentUser): ?>
+                    <h4>
+                        Merry meet,
+                        <?= htmlspecialchars($currentUser['user_first_name'] ?? 'Guest') ?>
+                        <?= htmlspecialchars($currentUser['user_last_name'] ?? '') ?>!
+                    </h4>
+
+                    <button class="dropdown-toggle" id="drop-down">
+                        User dropdown ➤
+                    </button>
+
+                    <ul class="user-dropdown">
+                        <li><a href="/profile"><?= LocalizationHelper::get("user_dropdown_content.option1") ?></a></li>
+                        <li><a href="/wishlist"><?= LocalizationHelper::get("user_dropdown_content.option2") ?></a></li>
+                        <li><a href="/cart"><?= LocalizationHelper::get("user_dropdown_content.option3") ?></a></li>
+                        <li><a href="/orders"><?= LocalizationHelper::get("user_dropdown_content.option4") ?></a></li>
+                        <li><a href="/settings"><?= LocalizationHelper::get("user_dropdown_content.option5") ?></a></li>
+                        <li><a href="/sign-out"><?= LocalizationHelper::get("user_dropdown_content.option6") ?></a></li>
+                    </ul>
+
+                    <?php if (!empty($currentUser['user_pfp_src'])): ?>
+                        <a href="/profile">
+                            <img
+                                id="pfp"
+                                src="<?= htmlspecialchars($currentUser['user_pfp_src']) ?>"
+                                alt="Profile picture"
+                            >
+                        </a>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <h5>Merry meet, Anonymous one!</h5>
+
+                    <button class="dropdown-toggle" id="drop-down">
+                        User dropdown
+                    </button>
+
+                    <ul class="user-dropdown">
+                        <li><a href="/sign-in"><?= LocalizationHelper::get("user_dropdown_content.signintext") ?></a></li>
+                        <li><a href="/sign-up"><?= LocalizationHelper::get("user_dropdown_content.signouttext") ?></a></li>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+    </nav>
+</header>
